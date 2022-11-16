16de0c7a5f4fb55dd8361652d322cf1a3aaa2b0d
David Angulo <dfangulo@gmail.com>
2022-11-16 23:38:40 +0000

    agegmos algunso archivos de configuracion, y un formulario para comenzar a subir datos a una base de datos, tambien pusimos las variables para ello

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php 
+    define('URL','http://curso-php.com/mvc/');
+    define('HOST', '192.168.122.218');
+    define('USERNAME', 'oa3db');
+    define('PASSWORD', 'Lanix2012$');
+    define('DB', 'oa3');
+    define('CHARSET','utf8');
+?>
