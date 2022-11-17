4711003c20e0642753b470fa816307105811a5ac
David Angulo <dfangulo@gmail.com>
2022-11-17 20:29:02 +0000

    Se agregaron al consulta de datos y la insercion de datos

--- models/nuevomodel.php
+++ models/nuevomodel.php
@@ -5,10 +5,19 @@
             parent::__construct();
         }
 
-        public function insert()
+        public function insert($datos)
         {
             //insertar datos en la base de datos
-            echo "<br/>Insertar datos";
+            try {
+                $query = $this->db->connect()->prepare('INSERT INTO alumnos (nombre, apellido) VALUES (:nombre, :apellido)');
+                if($query->execute(['nombre' => $datos['nombre'], 'apellido' => $datos['apellido']])){
+                    echo '<div class="center color-2"> Alumno agregado: '. $datos['nombre'] . ' '. $datos['apellido'] . '</div>';
+                }else{
+                    echo "error";
+                }
+            } catch(PDOException $e){
+                echo $e->getMessage();
+            }
         }
     }
 
